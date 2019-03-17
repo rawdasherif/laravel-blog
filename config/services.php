c02a61411ec69c3f2d71c3d8471172e3ce7ffb71
Rawda Sherif <rawdasherif119@gmail.com>
2019-03-17 22:17:30 +0200

    add socialite

--- config/services.php
+++ config/services.php
@@ -44,4 +44,10 @@ return [
         ],
     ],
 
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => 'http://127.0.0.1:8000/login/github/callback',
+    ],
+
 ];
